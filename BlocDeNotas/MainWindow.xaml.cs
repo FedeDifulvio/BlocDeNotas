3f44a9d2903f50128bb24197daa01fd3adf27a6a
FedeDifulvio <71615110+FedeDifulvio@users.noreply.github.com>
2021-01-12 16:52:00 -0300

    Pestaña Archivos
    Nuevo, abrir, guardar y guardar como.

--- BlocDeNotas/MainWindow.xaml.cs
+++ BlocDeNotas/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32; 
 
 namespace BlocDeNotas
 {
@@ -20,9 +22,93 @@ namespace BlocDeNotas
     /// </summary>
     public partial class MainWindow : Window
     {
+        string archivoAbierto = null; 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Clear();
+            archivoAbierto = null;
+        }
+
+        private void abrir_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            //Representa un cuadro de diálogo común que permite a un usuario especificar un nombre de archivo para abrir uno o más archivos.
+            try
+            {
+                open.Filter = "Archivo de texto (.txt)|*.txt";
+                open.Title = "Abrir archivo";
+                open.ShowDialog();  //Muestra la ventana
+                open.OpenFile();
+                archivoAbierto = open.FileName;
+                StreamReader lectura = File.OpenText(open.FileName);
+                TextBox.Text = lectura.ReadToEnd();
+                lectura.Close();
+                open.Reset();
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error al abrir el archivo"  + err.Message); 
+            }
+            
+            
+        }
+
+        private void guardar_Click(object sender, RoutedEventArgs e)
+        {   
+            try
+            {
+                if(!File.Exists(archivoAbierto))
+                {
+                    SaveAs();
+                }
+                else
+                {
+                    TextWriter overWriter = new StreamWriter(archivoAbierto);
+                    overWriter.Write(TextBox.Text);
+                    overWriter.Flush();
+                    overWriter.Close(); 
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error al guardar"  + error.Message); 
+            }
+            
+
+        }
+
+        private void guardarComo_Click(object sender, RoutedEventArgs e)
+        {
+            SaveAs();  
+        } 
+
+        private void SaveAs()
+        {
+            SaveFileDialog saveAs = new SaveFileDialog();
+
+            try
+            {
+                saveAs.Filter = "Archivo de texto (.txt)|*.txt";
+                saveAs.Title = "Guardar como...";
+                saveAs.ShowDialog();
+                StreamWriter writer = File.CreateText(saveAs.FileName);
+                archivoAbierto = saveAs.FileName;
+                writer.Write(TextBox.Text);
+                writer.Flush();
+                writer.Close();
+               
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error al guardar" + e.Message);
+            }
         }
     }
 }
